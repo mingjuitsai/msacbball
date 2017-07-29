280a0afade612445114cc9fccd755edf8deb44e3
Ray Tsai <connect@ray-tsai.com>
2017-07-29 15:27:22 +1000

    Optimise the phantom JS async callback

--- app.js
+++ app.js
@@ -2,37 +2,38 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 const phantom = require('phantom');
 
-var data_url = 'https://secure.activecarrot.com/public/facility/iframe_read_only/33/778/2017-06-19';
-
-
-function phantomOpenContent(url, callback) {
-  phantom.create().then(function(phInstance) {
-    phInstance.createPage().then(function(page) {
-      page.open(url).then(function(status){
-        if(status === 'success') {
-          page.property('content').then(function(content){
-            callback({
-              instance: phInstance,
-              page: page,
-              content: content
-            });
-          });
-        } else {
-          console.warn('Phantom open URL but something went wrong.');
-        }
-      });
-    });
-  });
-}
+var msac_url = 'https://secure.activecarrot.com/public/facility/iframe_read_only/33/778/2017-06-19';
 
 /**
- * Build bball fetched data
+ * Open url with phantom JS
+ * @param  {url} url URL to open
+ * @return {content}     return content html if success
  */
-function buildMsacData(phData) {
-  // Set variables
-  let phInstance = phData.instance,
-  page = phData.page,
-  content = phData.content; 
+async function phantomOpen(url) {
+    const instance = await phantom.create();
+    const page = await instance.createPage();
+    const status = await page.open(url);
+    console.log(status);
+
+    if( status === 'success') {
+      const content = await page.property('content');
+      await instance.exit();
+      return content;
+    } else {
+      console.warn('Phantom opened url but something went wrong.');
+    }
+
+    await instance.exit();
+};
+
+phantomOpen(msac_url).then( content => {
+  buildMsacData(content);
+});
+
+// /**
+//  * Build bball fetched data
+//  */
+function buildMsacData(content) {
 
   // Build data
   var data = {
@@ -43,13 +44,32 @@ function buildMsacData(phData) {
     data.court1.push($(this).text());
   });
 
-  console.log(data.court1.reverse());
-
-  page.close();
-  phInstance.exit();
+  console.log(data.court1.reverse());  
 }
 
+/* JSON
 
-phantomOpenContent(data_url, function(phData){
-  buildMsacData(phData);
-});
+
+[
+  09-10-2017: {
+    courts: [
+      {
+        number: 1,
+        available: [
+          {
+            start: timestamp,
+            end: timestamp
+          },
+          {
+            start: timestamp,
+            end: timestamp
+          }
+        ]
+      }
+    ]
+  }
+]
+
+
+
+*/
