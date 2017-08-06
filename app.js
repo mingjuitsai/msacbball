0e0a68e19911b9c0e600fe4f1c112dcf4a842cb2
Ray Tsai <connect@ray-tsai.com>
2017-08-06 16:22:15 +1000

    Add comments to app js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 
+/**
+ * Global variables
+ */
 const jsonfile = require('jsonfile');
 var getMsacData = require('./scripts/getMsacData');
 
-
+/**
+ * Fetch MSAC data then save json
+ */
 getMsacData.then(function(data) {
   jsonfile.writeFile('./data.json', data, function(err) {
     console.error(err)
