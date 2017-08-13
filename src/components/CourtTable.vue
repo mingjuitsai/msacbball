3e0d83cd2fbe7ed02dc8ccb811ab52933af69b2c
Ray Tsai <connect@ray-tsai.com>
2017-08-13 21:47:03 +1000

    Port data into table

--- src/components/CourtTable.vue
+++ src/components/CourtTable.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="courtsTable">
-  </div>
+  <ul class="courtsTable">
+    <li class="court" v-for="court in courts">
+      {{ court.id }}
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -8,10 +11,16 @@
 export default {
   name: 'courtsTable',
   props: ['courtsData'],
+  data () {
+    return {
+      courts: null,
+    }
+  },
   created: function() {
     var vm = this;
     vm.courtsData.then(function(data){
-      console.log(data);
+      console.table(data[0].unavailable);
+      vm.courts = data;
     });
   }
 }
