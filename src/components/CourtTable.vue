19487c704a5c00bdcadc6d9b9a9b7a402c260eb1
Peter <pvl.development@gmail.com>
2017-08-17 10:28:54 +1000

    added some classes for timeslot (depends on isAvailable function) and setup some temp styling

--- src/components/CourtTable.vue
+++ src/components/CourtTable.vue
@@ -1,10 +1,29 @@
 <template>
-  <ul class="courtsTable">
-    <li class="court" v-for="court in courts">
-      {{ court.id }}
-    </li>
-  </ul>
+  <div class="courtsTable">
+    <section class="court" v-for="court in courts" v-bind:key="court">
+      <aside class="time-slot" v-bind:class="isAvailable(getTimeslotTimeStamp(timeslot)) ? 'time-slot--available' : 'time-slot--unavailable'" v-for="timeslot in timeslotLength" v-bind:key="timeslot"></aside>
+    </section>
+  </div>
 </template>
+
+<style lang='scss'>
+  .court{
+    width: percentage(1/8);
+    display: inline-block;
+  }
+
+  .time-slot{
+    min-height: 50px;
+    border: 1px solid #000;
+    &--available{
+      background-color: teal;
+    }
+
+    &--unavailable{
+      background-color: #ddd;
+    }
+  }
+</style>
 
 <script>
 
@@ -14,6 +33,17 @@ export default {
   data () {
     return {
       courts: null,
+      timeslotLength: 56, // ((12 - 6) + (20 - 12)) * 2 * 2,
+      courtsStartTime: '6:00am',
+      courtsEndTime: '8:00pm'
+    }
+  },
+  methods: {
+    getTimeslotTimeStamp: function(timeslot) {
+      
+    },
+    isAvailable: function(timestamp) {
+      return Math.random() > 0.5;
     }
   },
   created: function() {
